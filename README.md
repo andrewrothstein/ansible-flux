4f365f420ef496ef4c0266a9a456f929a2d9d60f
Andrew Rothstein <andrew.rothstein@gmail.com>
2022-04-26 21:15:11 -0400

    flux:0.29.3

---
+++ README.md
@@ -0,0 +1,40 @@
+andrewrothstein.flux
+=========
+
+![Build Status](https://github.com/andrewrothstein/ansible-flux/actions/workflows/build.yml/badge.svg)
+
+Installs [flux2](https://github.com/fluxcd/flux2)
+
+Requirements
+------------
+
+See [meta/main.yml](meta/main.yml)
+
+Role Variables
+--------------
+
+See [defaults/main.yml](defaults/main.yml)
+
+Dependencies
+------------
+
+See [meta/main.yml](meta/main.yml)
+
+Example Playbook
+----------------
+
+```yml
+- hosts: servers
+  roles:
+    - andrewrothstein.flux
+```
+
+License
+-------
+
+MIT
+
+Author Information
+------------------
+
+Andrew Rothstein <andrew.rothstein@gmail.com>
---
+++ dl-checksum.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env sh
+set -e
+DIR=~/Downloads
+MIRROR=https://github.com/fluxcd/flux2/releases/download
+APP=flux
+
+dl()
+{
+    local ver=$1
+    local os=$2
+    local arch=$3
+    local archive_type=${4:-tar.gz}
+    local platform="${os}_${arch}"
+    local file="${APP}_${ver}_${platform}.${archive_type}"
+    local url=$MIRROR/v$ver/$file
+    local lfile=$DIR/$file
+
+    if [ ! -e $lfile ];
+    then
+        curl -sSLf -o $lfile $url
+    fi
+
+    printf "    # %s\n" $url
+    printf "    %s: sha256:%s\n" $platform $(sha256sum $lfile | awk '{print $1}')
+}
+
+dl_ver() {
+    local ver=$1
+    printf "  '%s':\n" $ver
+    dl $ver darwin amd64
+    dl $ver linux amd64
+    dl $ver linux arm
+    dl $ver windows amd64 zip
+    dl $ver windows arm64 zip
+    dl $ver windows 386 zip
+}
+
+dl_ver ${1:-0.29.3}
